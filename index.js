215d93404b97fd840613298ad64c203257f0efde
Victoria <vep002@gmail.com>
2021-04-28 12:42:22 -0500

    commit tree f52e36ef5af23dee9ad3e31b271642aafaa4de9a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block>42){ 
+    return `${block}`-42
+    } else {
+        return 42-`${block}`
+    }
+}
+function distanceFromHqInFeet (block) {
+    return distanceFromHqInBlocks(block)*264
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (destination>start){
+    return (`${destination}` - `${start}`)*264
+    } else {
+        return (`${start}`- `${destination}`)*264
+    }
+}
+function calculatesFarePrice(start, destination) {
+    let fare = distanceTravelledInFeet(start, destination)
+    if (fare<400) {
+        return 0
+    } else if (fare>400 && fare<2000){
+        return ((fare-400)*.02)
+    } else if (fare>2000 && fare<2500) {
+        return 25
+    } else if (fare>2500){
+        return 'cannot travel that far'
+    }
+}
